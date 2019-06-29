b1edab3238b039c3ae2312f1c9fd47c4c0545117
Alex Mota <alexmota5@hotmail.com>
2019-06-29 18:43:04 -0300

    change parte7

--- estoque/src/main/java/br/com/srm/Application.java
+++ estoque/src/main/java/br/com/srm/Application.java
@@ -6,13 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @RefreshScope
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "br.com.srm.repository")
 @EnableHystrix
 @EnableResourceServer
 @EnableBinding(CustomChannels.class)
